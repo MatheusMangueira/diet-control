1558e9047a8c880e1b61f411628afacd2cdf4a73
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-06-03 11:47:10 -0300

    feat: project initial

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { PersonInformation } from "./components/PersonInformation/PersonInformation";
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <PersonInformation />
+    </main>
+  );
+}
