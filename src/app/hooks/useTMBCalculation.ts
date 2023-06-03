386eec2b2965ca86017b23b4bdd607516450654f
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-06-03 11:52:06 -0300

    feat: hook creation to calculate TMB

---
+++ src/app/hooks/useTMBCalculation.ts
@@ -0,0 +1,72 @@
+"use client";
+import { useState } from "react";
+import { IFormInputs } from "../components/PersonInformation/PersonInformation";
+
+type TMBResult = string | undefined;
+
+type TMBCalculator = {
+  calculateTMB: (data: IFormInputs) => void;
+  result: TMBResult;
+};
+
+const useTMBCalculation = (): TMBCalculator => {
+  const [result, setResult] = useState<TMBResult>(undefined);
+
+  const calculateTMB = (data: IFormInputs) => {
+    const getActivityMultiplier = (activity: string): number => {
+      const activityMultipliers: { [key: string]: number } = {
+        Sedentário: 1.2,
+        "Levemente ativo": 1.375,
+        "Moderadamente ativo": 1.55,
+        "Muito ativo": 1.725,
+        "Extremamente ativo": 1.9,
+      };
+
+      return activityMultipliers[activity];
+    };
+
+    const getObjectiveValue = (objective: string): number => {
+      const objectiveValues: { [key: string]: number } = {
+        Emagrecer: 500,
+        "Manter peso": 0,
+        "Ganho de peso": 500,
+      };
+
+      return objectiveValues[objective];
+    };
+
+    const calculateTMBForMale = (): number => {
+      const tmb =
+        getActivityMultiplier(data.activity) *
+          (66 +
+            13.7 * Number(data.Weight) +
+            5 * Number(data.height) -
+            6.8 * Number(data.age)) -
+        getObjectiveValue(data.objective);
+
+      return tmb;
+    };
+
+    const calculateTMBForFemale = (): number => {
+      const tmb =
+        getActivityMultiplier(data.activity) *
+        (665 +
+          9.6 * Number(data.Weight) +
+          1.8 * Number(data.height) -
+          4.7 * Number(data.age));
+
+      return tmb;
+    };
+
+    const tmb =
+      data.sex === "Masculino"
+        ? calculateTMBForMale()
+        : calculateTMBForFemale();
+
+    setResult(tmb.toFixed(2));
+  };
+
+  return { result, calculateTMB };
+};
+
+export default useTMBCalculation;
