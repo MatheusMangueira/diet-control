9ec924cd1d5d58f6a0b925738262038cfcc01b71
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-06-13 20:52:07 -0300

    fix: adding the error to the components

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,15 +1,22 @@
 type IPropsButton = {
   title?: string;
+  disabled?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ title = "Click me", ...rest }: IPropsButton) => {
+export const Button = ({
+  disabled,
+  title = "Click me",
+  ...rest
+}: IPropsButton) => {
   return (
     <button
       {...rest}
-      className="py-2 px-4 bg-[#B0CC9C] text-white text-sm
+      className={`py-2 px-4 bg-[#B0CC9C] text-white text-sm
     rounded-md hover:bg-[#A0BC8C] transition duration-300 ease-in-out
-    min-w-[160px] shadow-sm hover:shadow-lg cursor-pointer 
-    "
+    min-w-[160px] shadow-sm hover:shadow-lg ${
+      disabled ? " cursor-not-allowed" : "cursor-pointer"
+    }
+    `}
     >
       {title}
     </button>
--- src/app/components/Inputs/Inputs.tsx
+++ src/app/components/Inputs/Inputs.tsx
@@ -1,15 +1,22 @@
 type IInputTextProps = {
   InputType?: string;
   label: string;
+  errors?: boolean;
 } & React.InputHTMLAttributes<HTMLInputElement>;
-type IInputSelectProps = {} & React.InputHTMLAttributes<HTMLSelectElement>;
+type IInputSelectProps = {
+  errors?: boolean;
+} & React.InputHTMLAttributes<HTMLSelectElement>;
 
-export const Input = ({ label, InputType, ...rest }: IInputTextProps) => {
+export const Input = ({
+  errors,
+  label,
+  InputType,
+  ...rest
+}: IInputTextProps) => {
   return (
     <div className="flex flex-col gap-4">
       <label
-        className="text-zinc-700 text-sm font-bold        
-      "
+        className={errors ? " text-red-600" : "text-zinc-700 text-sm font-bold"}
       >
         {label}
       </label>
@@ -23,7 +30,7 @@ export const Input = ({ label, InputType, ...rest }: IInputTextProps) => {
               key !== "Delete" &&
               key !== "ArrowLeft" &&
               key !== "ArrowRight" &&
-              key !== "Tab"&&
+              key !== "Tab" &&
               key !== "Enter"
             ) {
               event.preventDefault();
@@ -31,24 +38,27 @@ export const Input = ({ label, InputType, ...rest }: IInputTextProps) => {
           }
         }}
         {...rest}
-        className="py-2 px-4  text-zinc-700 text-sm border border-zinc-700 rounded-md hover:border-zinc-800 transition duration-300 ease-in-out w-full shadow-sm hover:shadow-lg "
+        className={`py-2 px-4  text-zinc-700 text-sm ${
+          errors ? "border-red-600 border" : "border border-zinc-700"
+        } rounded-md hover:border-zinc-800 transition duration-300 ease-in-out w-full shadow-sm hover:shadow-lg `}
       />
     </div>
   );
 };
 
-export const InputSelect = ({ ...rest }: IInputSelectProps) => {
+export const InputSelect = ({ errors, ...rest }: IInputSelectProps) => {
   return (
     <div className="flex flex-col gap-4">
       <label
-        className="text-zinc-700 text-sm font-bold        
-      "
+        className={errors ? "text-red-600" : "text-zinc-700 text-sm font-bold"}
       >
         {rest.placeholder}
       </label>
       <select
         {...rest}
-        className="py-2 px-4  text-zinc-700 text-sm border border-zinc-700 rounded-md hover:border-zinc-800 transition duration-300 ease-in-out w-full shadow-sm hover:shadow-lg "
+        className={`py-2 px-4  text-zinc-700 text-sm ${
+          errors ? "border-red-600 border" : "border border-zinc-700"
+        }  rounded-md hover:border-zinc-800 transition duration-300 ease-in-out w-full shadow-sm hover:shadow-lg`}
       >
         {rest.children}
       </select>
