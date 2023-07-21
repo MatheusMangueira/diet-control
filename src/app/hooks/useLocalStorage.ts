82849d97acec0d6760a7b8df27b85acc71078585
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-07-20 23:06:58 -0300

    bug-fix: localStorage use client

--- src/app/hooks/useLocalStorage.ts
+++ src/app/hooks/useLocalStorage.ts
@@ -1,21 +1,22 @@
+"use client";
+
 const useLocalStorage = (key: string, initialValue: any) => {
-   const isBrowser = typeof window !== 'undefined';
 
    const getLocalStorage = () => {
-      if (isBrowser) {
-         const item = localStorage.getItem(key);
-         if (item) {
-            return JSON.parse(item);
-         }
+
+      const item = localStorage.getItem(key);
+      if (item) {
+         return JSON.parse(item);
       }
+
       return initialValue;
    }
 
    const setLocalStorage = (value: any) => {
-      if (isBrowser) {
-         const item = localStorage.setItem(key, JSON.stringify(value));
-         return item;
-      }
+
+      const item = localStorage.setItem(key, JSON.stringify(value));
+      return item;
+
    }
 
    return { getLocalStorage, setLocalStorage };
